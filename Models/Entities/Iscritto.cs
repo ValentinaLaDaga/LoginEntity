b76b7f5c88f19a13f2c4502f5574e750b27e5328
Giorgi-v <giorgiavalenti97@gmail.com>
2024-05-11 15:29:14 +0200

    Entity Framework Installazione

---
+++ Models/Entities/Iscritto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LoginEntity.Models.Entities
+{
+    public partial class Iscritto
+    {
+        public long Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Nazione { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Models/Services/Infrastructure/LoginDbContext.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace LoginEntity.Models.Services.Infrastructure
+{
+    public partial class LoginDbContext : DbContext
+    {
+        public LoginDbContext()
+        {
+        }
+
+        public LoginDbContext(DbContextOptions<LoginDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Iscritto> Iscritto { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlite("Data Source=Data/Utenti.db");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Iscritto>(entity =>
+            {
+                entity.ToTable("iscritto");
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("id")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasColumnName("email")
+                    .HasColumnType("VARCHAR(45)");
+
+                entity.Property(e => e.Nazione)
+                    .IsRequired()
+                    .HasColumnName("nazione")
+                    .HasColumnType("VARCHAR(45)");
+
+                entity.Property(e => e.Nome)
+                    .IsRequired()
+                    .HasColumnName("nome")
+                    .HasColumnType("VARCHAR(45)");
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasColumnName("password")
+                    .HasColumnType("VARCHAR(45)");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
